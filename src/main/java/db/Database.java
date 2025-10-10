4288412dc0855aba2b886e45624fc4a31902fc7f
Patryk Prentki <patryk.prentki90@gmail.com>
2025-10-10 23:36:15 +0200

    moved in to another package

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,3 +1,5 @@
+package db;
+
 import utils.Config;
 
 import java.sql.Connection;
--- src/test/java/pages/TablePage.java
+++ src/test/java/pages/TablePage.java
@@ -1,3 +1,5 @@
+package pages;
+
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.openqa.selenium.By;
