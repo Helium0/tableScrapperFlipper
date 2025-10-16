196063b61ad97d0b1a237bb4bb119d3fc4db1e06
Patryk Prentki <patryk.prentki90@gmail.com>
2025-10-16 20:49:46 +0200

    added constructor and injected dependency

--- src/main/java/db/Database.java
+++ src/main/java/db/Database.java
@@ -1,21 +1,27 @@
 package db;
 
+import interfaces.ConnectionProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import utils.Config;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
 
+    private ConnectionProvider connectionProvider;
     private final Logger logger = LogManager.getLogger(Database.class);
+
+    public Database(ConnectionProvider connectionProvider) {
+        this.connectionProvider = connectionProvider;
+    }
+
 
     public Connection connectionToDatabase() throws SQLException {
         logger.info("*** CONNECTING TO DATABASE ***");
         try {
-            return DriverManager.getConnection(
+            return connectionProvider.getDatabaseConnection(
                     Config.getDataBaseURL(),
                     Config.getDatabaseUser(),
                     Config.getDatabasePassword());
